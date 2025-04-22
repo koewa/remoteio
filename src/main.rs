8568216d0614e690730bef7f153b98a278b17801
koewa <63922516+koewa@users.noreply.github.com>
2025-04-22 14:25:04 +0200

    Try to get websockets working, failed

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 #[macro_use] extern crate rocket;
 use rocket_ws::{WebSocket, Stream};
-//use rocket::tokio::sync::broadcast;
-//use rocket::response::content;
-//use rocket::State;
-//use std::sync::Arc;
+use rocket::tokio::sync::broadcast;
+use rocket::State;
+use std::sync::Arc;
 
-//#[derive(Clone)]
-//struct WebSocketBroadcaster {
-//  sender: broadcast::Sender<String>,
-//}
+#[derive(Debug, Clone)]
+pub enum Error {
+  DivisionByZero,
+  NonPositiveLogarithm,
+  NegativeSquareRoot,
+}
+
+#[derive(Clone)]
+struct WebSocketBroadcaster {
+  sender: broadcast::Sender<String>,
+  message: Result<String, Error>
+}
 
 #[get("/hello/<name>/<age>")]
 fn helloage(name: &str, age: u8) -> String {
@@ -16,9 +23,10 @@ fn helloage(name: &str, age: u8) -> String {
 }
 
 #[get("/hello/<name>")]
-fn hello(name: &str/*, broadcaster: &State<Arc<WebSocketBroadcaster>>*/) -> String {
+fn hello(name: &str, broadcaster: &State<Arc<WebSocketBroadcaster>>) -> String {
   let message = format!("Hello, {}!\n", name);
-  //broadcaster.sender.send(message.clone()).ok();
+  broadcaster.sender.send(message.clone()).ok();
+  //broadcaster.message = message.clone();
   message
 }
 
@@ -28,27 +36,27 @@ fn login(login: &str) -> String {
 }
 
 #[get("/echo")]
-fn echo_stream(ws: WebSocket/*, broadcaster: &State<Arc<WebSocketBroadcaster>>*/) -> Stream!['static] {
+fn echo_stream(ws: WebSocket, broadcaster: &State<Arc<WebSocketBroadcaster>>) -> Stream!['static] {
   Stream! { ws =>
-    //let mut receiver = broadcaster.sender.subscribe();
-    //while let Ok(message) = receiver.recv().await {
-    //  broadcaster.sender.send(message).await.ok();
-    //}
     for await message in ws {
-      yield message?;
+      yield rocket_ws::Message(String::from("init"));
+      //yield broadcaster.message?;
+      //yield message?;
+      //yield Result<String, MathError>(broadcaster.message)?
     }
   }
 }
 
 #[launch]
 fn rocket() -> _ {
-  //let (sender, _) = broadcast::channel(100);
-  //let broadcaster = Arc::new(WebSocketBroadcaster { sender });
+  let (sender, _) = broadcast::channel(100);
+  let initstring : String = String::from("init");
+  let broadcaster = Arc::new(WebSocketBroadcaster { sender, message: Ok(initstring.clone())});
   rocket::build().mount("/", routes![
     hello,
     helloage,
     login,
     echo_stream]
   )
-  //.manage(broadcaster)
+  .manage(broadcaster)
 }
