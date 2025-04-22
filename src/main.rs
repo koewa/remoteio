a026cf152815aaf5cb71bc901643fee9d87ce1d9
koewa <63922516+koewa@users.noreply.github.com>
2025-04-22 14:25:04 +0200

    Try to use a struct as server (doesn't work atm)

--- src/main.rs
+++ src/main.rs
@@ -104,7 +104,13 @@ async fn main() {
         tx: tx.clone(),
     }));
 
-    let _service = service::setup_broadcaster(tx);
+    let service = service::setup_broadcaster(tx.clone());
+    //tokio::spawn(async move {
+    //  service.broadcast_task(tx, "bla").await;
+    //});
+    //tokio::spawn(async move {
+    //  service.broadcast_task(tx, "bla").await;
+    //});
 
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
     let listener = TcpListener::bind(addr).await.unwrap();
--- src/service.rs
+++ src/service.rs
@@ -3,35 +3,47 @@ use std::time::Duration;
 use tokio::sync::broadcast::Sender;
 
 // Task that broadcasts a message every 10 seconds
-async fn broadcast_task(tx: Sender<String>, name: &str) {
-    loop {
-        task::sleep(Duration::from_secs(1)).await;
-        let message = format!(
-            "Server broadcast: Hello from {} to all WebSocket clients!",
-            name
-        )
-        .to_string();
-        println!("Broadcasting message: {}", message);
-
-        if tx.send(message).is_err() {
-            println!(
-                "No active WebSocket clients to send the message to ({}).",
-                name
-            );
-        }
-    }
-}
-
 pub struct Service {}
 
-pub fn setup_broadcaster(tx: Sender<String>) -> Service {
-    let tx_clone = tx.clone();
-    tokio::spawn(async {
-        broadcast_task(tx_clone, "first").await;
-    });
-    tokio::spawn(async {
-        broadcast_task(tx, "second").await;
-    });
+impl Service {
+    fn new(tx: Sender<String>) -> Self {
+        let this = Self{};
 
-    Service {}
+        //let tx_clone = tx.clone();
+        //tokio::spawn(async {
+        //    this.broadcast_task(tx_clone, "first").await;
+        //});
+        //tokio::spawn(async {
+        //    this.broadcast_task(tx, "second").await;
+        //});
+        //
+        this.broadcast_task(tx, "second");
+        this
+    }
+
+    async start
+
+    pub async fn broadcast_task(&self, tx: Sender<String>, name: &str) {
+        loop {
+            task::sleep(Duration::from_secs(1)).await;
+            let message = format!(
+                "Server broadcast: Hello from {} to all WebSocket clients!",
+                name
+            )
+            .to_string();
+            println!("Broadcasting message: {}", message);
+    
+            if tx.send(message).is_err() {
+                println!(
+                    "No active WebSocket clients to send the message to ({}).",
+                    name
+                );
+            }
+        }
+    }
+
+}
+
+pub fn setup_broadcaster(tx: Sender<String>) -> Service {
+    Service::new(tx)
 }
