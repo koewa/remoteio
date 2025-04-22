8bc3076667cd7077d4debfa352c85a6e22cee3a7
koewa <63922516+koewa@users.noreply.github.com>
2025-04-22 14:25:04 +0200

    Add some code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 #[macro_use] extern crate rocket;
 use rocket_ws::{WebSocket, Stream};
+//use rocket::tokio::sync::broadcast;
+//use rocket::response::content;
+//use rocket::State;
+//use std::sync::Arc;
+
+//#[derive(Clone)]
+//struct WebSocketBroadcaster {
+//  sender: broadcast::Sender<String>,
+//}
 
 #[get("/hello/<name>/<age>")]
 fn helloage(name: &str, age: u8) -> String {
@@ -7,8 +16,9 @@ fn helloage(name: &str, age: u8) -> String {
 }
 
 #[get("/hello/<name>")]
-fn hello(name: &str) -> String {
+fn hello(name: &str/*, broadcaster: &State<Arc<WebSocketBroadcaster>>*/) -> String {
   let message = format!("Hello, {}!\n", name);
+  //broadcaster.sender.send(message.clone()).ok();
   message
 }
 
@@ -18,8 +28,12 @@ fn login(login: &str) -> String {
 }
 
 #[get("/echo")]
-fn echo_stream(ws: WebSocket) -> Stream!['static] {
+fn echo_stream(ws: WebSocket/*, broadcaster: &State<Arc<WebSocketBroadcaster>>*/) -> Stream!['static] {
   Stream! { ws =>
+    //let mut receiver = broadcaster.sender.subscribe();
+    //while let Ok(message) = receiver.recv().await {
+    //  broadcaster.sender.send(message).await.ok();
+    //}
     for await message in ws {
       yield message?;
     }
@@ -28,10 +42,13 @@ fn echo_stream(ws: WebSocket) -> Stream!['static] {
 
 #[launch]
 fn rocket() -> _ {
+  //let (sender, _) = broadcast::channel(100);
+  //let broadcaster = Arc::new(WebSocketBroadcaster { sender });
   rocket::build().mount("/", routes![
     hello,
     helloage,
     login,
     echo_stream]
   )
+  //.manage(broadcaster)
 }
