98d0ef0d1d5e1baa5466eeaa4841810f18411d63
koewa <63922516+koewa@users.noreply.github.com>
2025-04-22 17:18:48 +0200

    It doesn't make sense to have a service struct

--- src/service.rs
+++ src/service.rs
@@ -2,9 +2,6 @@ use async_std::task;
 use std::time::Duration;
 use tokio::sync::broadcast::{channel, Receiver, Sender};
 
-// Task that broadcasts a message every 10 seconds
-pub struct Service {}
-
 fn run_timer(name: String, tx: Sender<String>, interval: Duration) {
     tokio::spawn(async move {
         loop {
@@ -16,39 +13,32 @@ fn run_timer(name: String, tx: Sender<String>, interval: Duration) {
     });
 }
 
-impl Service {
-    fn new(tx: Sender<String>) {
-        let (txtask, _) = channel::<String>(10);
-        let this = Self {};
+pub async fn service(mut rx: Receiver<String>, tx: Sender<String>) {
+    loop {
+        if let Ok(name) = rx.recv().await {
+            let msg = format!(
+                "Server broadcast: Hello from server to all WebSocket clients ({})!",
+                name
+            )
+            .to_string();
+            println!("Broadcasting message: {}", msg);
 
-        run_timer("first".to_string(), txtask.clone(), Duration::from_secs(2));
-        run_timer("second".to_string(), txtask.clone(), Duration::from_secs(3));
-
-        tokio::spawn(async move {
-            this.broadcast_task(txtask.subscribe(), tx).await;
-        });
-    }
-
-    pub async fn broadcast_task(self, mut rx: Receiver<String>, tx: Sender<String>) {
-        loop {
-            if let Ok(name) = rx.recv().await {
-                let msg = format!(
-                    "Server broadcast: Hello from server to all WebSocket clients ({})!",
-                    name
-                )
-                .to_string();
-                println!("Broadcasting message: {}", msg);
-
-                if tx.send(msg).is_err() {
-                    println!("No active WebSocket clients to send the message to!",);
-                }
-            } else {
-                println!("Some error occured");
+            if tx.send(msg).is_err() {
+                println!("No active WebSocket clients to send the message to!",);
             }
+        } else {
+            println!("Some error occured");
         }
     }
 }
 
 pub fn setup_broadcaster(tx: Sender<String>) {
-    Service::new(tx);
+    let (txtask, _) = channel::<String>(10);
+
+    run_timer("first".to_string(), txtask.clone(), Duration::from_secs(2));
+    run_timer("second".to_string(), txtask.clone(), Duration::from_secs(3));
+
+    tokio::spawn(async move {
+        service(txtask.subscribe(), tx).await;
+    });
 }
